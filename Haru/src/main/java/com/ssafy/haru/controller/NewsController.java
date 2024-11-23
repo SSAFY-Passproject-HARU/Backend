c105c19ce3e363b7876b1fcbcbd68a22c75556c7
Yunsun Jung <yunsun992510@gmail.com>
2024-11-23 22:32:07 +0900

    [Feat] 네이버 뉴스 검색 api 구현

---
+++ Haru/src/main/java/com/ssafy/haru/controller/NewsController.java
@@ -0,0 +1,25 @@
+package com.ssafy.haru.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.haru.model.response.NewsResponseDto;
+import com.ssafy.haru.service.NewsService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequiredArgsConstructor
+public class NewsController {
+    private final NewsService newsService;
+    
+    @GetMapping("/api/news")
+    public NewsResponseDto searchNews(@RequestParam String query,
+                             @RequestParam(defaultValue = "20") int size,
+                             @RequestParam(defaultValue = "0") int page) {
+        return newsService.searchNews(query, size, page);
+    }
+    
+}
