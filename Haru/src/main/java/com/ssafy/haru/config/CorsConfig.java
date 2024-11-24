4096b83d8111766132ee816e7208b5e5d97c958a
Yunsun Jung <yunsun992510@gmail.com>
2024-11-25 04:45:27 +0900

    [Feat] cors 처리 및 UserLoginRequestDto 조정

---
+++ Haru/src/main/java/com/ssafy/haru/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.haru.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173"); // 프론트엔드 주소
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.setAllowCredentials(true); // 쿠키 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
